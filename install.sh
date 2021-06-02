3ae13bd7a86f23167caf7651716de47ff75ea219
thoonly <thoonly@hotmail.com>
2021-06-02 11:12:20 +0700

    update repository

--- install.sh
+++ install.sh
@@ -369,7 +369,7 @@
     
     # Compil Blocknotify
     cd ~
-    hide_output git clone https://github.com/Kudaraidee/yiimp
+    hide_output git clone https://github.com/thoonly/yiimp
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     hide_output sudo make
